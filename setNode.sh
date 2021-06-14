659eb5a1d0dfdfd6cd82a19a8d5ec0cead9baff9
ShallyLai <79987875+ShallyLai@users.noreply.github.com>
2021-06-14 23:41:04 +0800

    v7

--- setNode.sh
+++ setNode.sh
@@ -2,15 +2,12 @@ cd ./node_modules/node-zookeeper-client/examples
 node create.js 127.0.0.1:2181 /test1
 node set.js 127.0.0.1:2181 /test1 57
 
-cd ./node_modules/node-zookeeper-client/examples
 node create.js 127.0.0.1:2181 /test2
 node set.js 127.0.0.1:2181 /test2 87
 
-cd ./node_modules/node-zookeeper-client/examples
 node create.js 127.0.0.1:2181 /test3
 node set.js 127.0.0.1:2181 /test3 38
 
-cd ./node_modules/node-zookeeper-client/examples
 node create.js 127.0.0.1:2181 /test4
 node set.js 127.0.0.1:2181 /test4 35
 
